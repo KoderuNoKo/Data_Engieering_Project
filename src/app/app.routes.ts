8615f9729b64ac1845a44629c6c49cd7fbcf717f
AnVai <kensan010204@gmail.com>
2024-12-08 16:31:27 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LogInComponent } from './components/log-in/log-in.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SignUpComponent } from './components/signup/signup.component';
 import { TestUiComponent } from './components/test-ui/test-ui.component';
+import { BacktestComponent } from './components/backtest/backtest.component';
 export const routes: Routes = [
     {path : '', component: LogInComponent},
     {path : 'homepage', component:LandingPageComponent},
     {path : 'signup', component: SignUpComponent},
-    {path : 'test', component: TestUiComponent}
+    {path : 'test', component: TestUiComponent},
+    {path : 'backtest', component: BacktestComponent}
 ];
